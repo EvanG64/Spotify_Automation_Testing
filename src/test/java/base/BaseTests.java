f80d3a2f9c14a033721461c2e59f989f175240fc
Evan Goudy <ebgoudy4412@eagle.fgcu.edu>
2026-03-26 17:30:10 -0400

    Initial commit

---
+++ src/test/java/base/BaseTests.java
@@ -0,0 +1,32 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+
+public class BaseTests {
+
+    protected WebDriver driver;
+    protected final String BASE_URL = "https://open.spotify.com/";
+
+    @BeforeMethod
+    public void setUp() {
+        // If using WebDriverManager, you can set it up here instead of system property
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get(BASE_URL);
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
+
